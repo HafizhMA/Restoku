ba4ff03885f1c3fef3e7d7df7cda21f75bb3504c
HafizhMA <muhammadazhr29@gmail.com>
2023-08-21 18:33:09 +0700

    membuat favorite page

---
+++ src/Pages/FavoritePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FavoritePage = () => {
+  return (
+    <>
+      <Header />
+      <section id="favoritePageSection"></section>
+      <Footer />
+    </>
+  );
+};
+
+export default FavoritePage;
